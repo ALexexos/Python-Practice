edf5d34bcfdc2a60bf00ffbe413b969b0c3a0f23
ALexexos <108174553+ALexexos@users.noreply.github.com>
2022-06-25 10:52:18 +0200

    Second commit
    Mistake has been fixed in forth line.

--- solvequad.py
+++ solvequad.py
@@ -1,7 +1,7 @@
 a = int(input("Enter first koefficient: "))
 b = int(input("Enter second koefficient: "))
 c = int(input("Enter third koefficient: "))
-D = b**2 - 4  a  c
+D = b**2 - 4 * a * c
 if D > 0:
     x1 = (- b - D**0.5) / (2 * a)
     x2 = (- b + D**0.5) / (2 * a)
